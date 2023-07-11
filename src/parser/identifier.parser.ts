ec8a4fc9fbd330eb8c3f477a8925bb8f89622a59
Dominik Mielcarek <work@mielcarek.dev>
2023-07-11 03:53:34 +0200

    Implemented identifier parser

---
+++ src/parser/identifier.parser.ts
@@ -0,0 +1,25 @@
+import { ConsumeWhitespace, isWhitespaceConsumed } from './whitespace.parser';
+import { AllowedDelimiter, IdentifierAllowedChars, IdentifierAllowedStartChars } from './constants';
+import { ParserError } from '../errors';
+import { Head } from '../helpers';
+
+
+/**
+ * @param {string} source
+ * @returns [identifier: string, sourceTail: string]
+ */
+export type ParseIdentifier<Source extends string> =
+    isWhitespaceConsumed<Source> extends false ?
+        ParseIdentifier<ConsumeWhitespace<Source>>
+    : Source extends `${infer char extends IdentifierAllowedStartChars}${string}` ?
+        ParseIdentifierRest<Source>
+    : ParserError<`Expected identifier, got ${Head<Source>}`>
+
+type ParseIdentifierRest<Source extends string> =
+    Source extends `${infer char extends IdentifierAllowedChars}${infer tail}` ?
+        ParseIdentifierRest<tail> extends [infer identifierRest extends string, infer tail2] ?
+            [`${char}${identifierRest}`, tail2]
+        : ParseIdentifierRest<tail> // error pass-through
+    : Source extends `${AllowedDelimiter}${string}` | '' ?
+        ['', Source]
+    : ParserError<`Cannot parse unexpected character '${Head<Source>}'`>;
