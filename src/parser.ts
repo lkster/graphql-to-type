b6ee9e3aba5e6ee32a8e1f8a533a47a90e93070a
Dominik Mielcarek <work@mielcarek.dev>
2023-07-18 01:10:08 +0200

    Added parser and transformer export types

---
+++ src/parser.ts
@@ -0,0 +1,13 @@
+export * from './parser/arguments.parser';
+export * from './parser/identifier.parser';
+export * from './parser/literal.parser';
+export * from './parser/operation.parser';
+export * from './parser/selection.parser';
+export * from './parser/type.parser';
+export * from './parser/whitespace.parser';
+export * from './parser/parser';
+
+export * from './parser/constants';
+export * from './parser/expression';
+
+export * from './errors';
---
+++ src/transformer.ts
@@ -0,0 +1,4 @@
+export * from './transformer/operation.transformer';
+export * from './transformer/selection.transformer';
+export * from './transformer/type.transformer';
+export * from './transformer/variables.transformer';
