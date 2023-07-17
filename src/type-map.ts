3db9165eefbca7a6020ad0c52cd6dd4c59346225
Dominik Mielcarek <work@mielcarek.dev>
2023-07-18 00:44:02 +0200

    Changed query property to be optional in TypeMap

--- src/type-map.ts
+++ src/type-map.ts
@@ -1,5 +1,5 @@
 export interface TypeMap {
-    query: Record<string, any>;
+    query?: Record<string, any>;
     mutation?: Record<string, any>;
     subscription?: Record<string, any>;
     types: Record<string, any>;
